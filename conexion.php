7f2a036790351f8476d00c6f659fb0c8c7bed9eb
j0su3-8 <josuefrainms@gmail.com>
2017-09-09 21:32:39 -0600

    conexion con base de datos

---
+++ conexion.php
@@ -0,0 +1,18 @@
+<?php  
+	/*	$conexion=new mysqli("localhost", "root", "", "subestaciones");*/
+
+function Conectarse()  
+{  
+   if (!($link=mysql_connect("localhost","n_usuario","password")))  
+   {  
+      echo "Error conectando a la base de datos.";  
+      exit();  
+   }  
+   if (!mysql_select_db("n_base",$link))  
+   {  
+      echo "Error seleccionando la base de datos.";  
+      exit();  
+   }  
+   return $link;  
+}  
+?>  # Estaciones
